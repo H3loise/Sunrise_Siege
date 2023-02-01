5efe6ff4f48641156550c475d79aa043361c8d9e
Héloïse <heloise.bieder@gmail.com>
2023-02-01 19:26:24 +0100

    Héloïse V 1.0

--- src/Map.java
+++ src/Map.java
@@ -2,15 +2,25 @@ import java.util.ArrayList;
 
 public class Map {
     private ArrayList<Obstacle> obstacles;
-    private ArrayList<Personnage> personnages;
+    private ArrayList<Personnage> characters;
     private ArrayList<Batiment> batiments;
     public static final int taille=600;
 
+    public ArrayList<Obstacle> getObstacles() {
+        return obstacles;
+    }
 
+    public ArrayList<Batiment> getBatiments() {
+        return batiments;
+    }
 
-    public Map(ArrayList<Obstacle> o, ArrayList<Personnage> p, ArrayList<Batiment> b){
+    public ArrayList<Personnage> getPersonnages() {
+        return characters;
+    }
+
+    public Map(ArrayList<Obstacle> o, ArrayList<Personnage> c, ArrayList<Batiment> b){
         this.batiments=b;
         this.obstacles=o;
-        this.personnages=p;
+        this.characters=c;
     }
 }

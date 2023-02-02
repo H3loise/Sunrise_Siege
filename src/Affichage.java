12f250c4d1fa7d32a77bc28160c0af4d2ef27951
WohaZz <bastien.rpr@gmail.com>
2023-02-02 20:23:05 +0100

    Classe Affichage

--- src/Affichage.java
+++ src/Affichage.java
@@ -1,3 +1,17 @@
-public class Affichage {
+import Batiments.Nexus;
 
+import javax.swing.*;
+import java.awt.*;
+
+public class Affichage extends JPanel {
+    public Nexus = new Nexus(100,500);;
+    public Affichage(){
+        setPreferredSize(new Dimension(600,600));
+        setBackground(Color.LIGHT_GRAY);
+    }
+
+    public void paint(Graphics g){
+        g.setColor(Color.BLACK);
+
+    }
 }
